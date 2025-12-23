1ad0e14ac74e70ecb5d9d1a11cb63554a92e7c00
Arpi Barsan <artimonvarga@gmail.com>
2025-12-23 21:05:49 +0100

    Add files via upload

---
+++ ElectroPlus.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const boton = document.getElementById("btn-auditoria");
+
+  boton.addEventListener("click", () => {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfJAiNSxpLv2h7qcMnhAQsL9iUJ8zvhnL7vEP4YNB24pxpnig/viewform?usp=dialog";
+  });
+});
