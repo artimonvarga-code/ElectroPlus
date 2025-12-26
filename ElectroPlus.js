ce89a8788efae4b6093b4a40842e127e8316ceec
Arpi Barsan <artimonvarga@gmail.com>
2025-12-26 16:51:03 +0100

    Add files via upload

--- ElectroPlus.js
+++ ElectroPlus.js
@@ -2,6 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const boton = document.getElementById("btn-auditoria");
 
   boton.addEventListener("click", () => {
-    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfJAiNSxpLv2h7qcMnhAQsL9iUJ8zvhnL7vEP4YNB24pxpnig/viewform?usp=dialog";
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSc8avAuJtjhIENV4Fy0yQHW5fZx65usFEQANkXZ2OT5bzv8zA/viewform?usp=header";
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btn-auditoria");
+
+  if(btn){
+    btn.addEventListener("click", () => {
+      alert("Gracias por tu interés 😄 Pronto te contactaremos");
+    });
+  }
+});
+
